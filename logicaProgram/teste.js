a84e730967409f8b2a5774261db402945f658be9
Adriana Balon <drizi.b.oliveira@gmail.com>
2025-11-08 19:17:18 -0300

    ADA

--- logicaProgram/teste.js
+++ logicaProgram/teste.js
@@ -1,9 +1,17 @@
-let prompt = require("prompt-sync") ();
-let maiorNumero = 0;
-let numeroInformado;
-for (let i = 1; i <= 5; i++) {
-    numeroInformado = prompt("Digite um numero: ")
-        if (numeroInformado > maiorNumero) {
-            maiorNumero = numeroInformado;
-    } 
-} console.log("O maior numero digitado foi: ", maiorNumero);
+// let prompt = require("prompt-sync") ();
+// let maiorNumero = 0;
+// let numeroInformado;
+// for (let i = 1; i <= 5; i++) {
+//     numeroInformado = prompt("Digite um numero: ")
+//         if (numeroInformado > maiorNumero) {
+//             maiorNumero = numeroInformado;
+//     } 
+// } console.log("O maior numero digitado foi: ", maiorNumero);
+
+const nome = "Welinton";
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+    if (i === 3) {
+        break
+    }
+}
