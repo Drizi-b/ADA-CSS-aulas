65f1a2343fc87cf1547005335c5981b56a46ee43
Adriana Balon <drizi.b.oliveira@gmail.com>
2026-01-16 22:44:31 -0300

    DOM

--- DOM/js/index.js
+++ DOM/js/index.js
@@ -7,7 +7,7 @@ console.log(todosH1);
 const todosClasseParagrafos = document.getElementsByClassName('paragrafo');
 console.log(todosClasseParagrafos);
 
-// 3. Pegar pelo nome da name
+// 3. Pegar pelo nome da 'name' do formulário
 const emailInput = document.getElementsByName('email');
 console.log(emailInput);
 
@@ -16,9 +16,36 @@ const jsLogoImg = document.getElementById('js-logo');
 console.log(jsLogoImg);
 
 // 5. Pegar usando querySelector (pega o primeiro elemento que combinar com o seletor)
-const imagem = document.querySelector('body > img');
+const imagem = document.querySelector('body > img'); // (logo abaixo do body, existe a tag img)
 console.log(imagem);
 
 // 6. Pegar usando querySelectorAll (pega todos os elementos que combinarem com o seletor)
 const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);   
+console.log(paragrafos);
+
+// Conferindo/Alterando o conteúdo da tag.
+const primeiroParagrafo = document.querySelector('p.paragrafo'); // seleciona o primeiro parágrafo com a classe (.)'paragrafo'
+console.log(primeiroParagrafo)
+console.log('textContent:', primeiroParagrafo.textContent); // ignora tags e mostra somente o texto, conteúdo em texto. 
+console.log('innerHTML:', primeiroParagrafo.innerHTML); // mostra o conteúdo HTML com tags e textos
+primeiroParagrafo.textContent = 'Outra coisa'
+
+console.clear();
+// Pegando um elemento em particular e coloca no formulario
+emailInput[0].value = 'drizi@email.com';
+
+// Criando elementos na pasta html
+const listaUl = document.querySelector('ul#lista');
+const listaLis = document.querySelectorAll('ul > li');// pega todas as lis que são filhas diretas da ul
+const novaTagLi = document.createElement('li'); //  cria uma nova tag li vazia
+novaTagLi.textContent = 'Quarto item'; // adiciona texto dentro da tag li
+console.log(novaTagLi);
+// listaUl.appendChild(novaTagLi); // adiciona a nova tag li(filha) dentro da ul(pai) no final da lista
+
+// Inserindo um elemento num lugar específico
+listaUl.insertBefore(novaTagLi, listaLis[3]);
+// dentro da listaUl inserir uma nova Tag, antes da posição 3
+
+// Remover elemento
+listaUl.removeChild(novaTagLi);
+// dentro da listaUl remover a novaTagLi
